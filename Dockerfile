3f91c2234cc889eb3f0204f80c24f1225fb9f10b
haholl <haholl@taltech.ee>
2024-11-24 22:55:12 +0200

    Fixed first-time startup

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,13 @@
-# Dockerfile
 FROM python:3.9-slim
 
-# Set the working directory
 WORKDIR /app
 
-# Copy the requirements file
 COPY requirements.txt .
 
-# Install dependencies
 RUN pip install -r requirements.txt
 
-# Copy the rest of the application code
 COPY . .
 
-CMD ["sh", "-c", "python populate_db_script.py && flask run --host=0.0.0.0"]
+COPY wait-for-it.sh /wait-for-it.sh
+
+CMD ["sh", "-c", "/wait-for-it.sh flask_db:5432 -- flask run --host=0.0.0.0"]
--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from tenacity import retry, wait_fixed, stop_after_attempt
+import os
 
 db = SQLAlchemy()
 
 
+@retry(wait=wait_fixed(2), stop=stop_after_attempt(5))
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.config.Config')
@@ -15,4 +18,11 @@ def create_app():
         app.register_blueprint(company_controller.bp)
         db.create_all()
 
+        os.system('python populate_db_script.py')
+
     return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(host='0.0.0.0')
